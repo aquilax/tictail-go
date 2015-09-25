8ddbdd0ccdb286c11d9273e9dcafe60577e46577
aquilax <aquilax@gmail.com>
2015-09-25 09:25:29 +0200

    formated auth

--- tictail_auth.go
+++ tictail_auth.go
@@ -8,24 +8,24 @@ import (
 )
 
 const (
-		AUTH_URL = "https://tictail.com/oauth/token"
+	AUTH_URL = "https://tictail.com/oauth/token"
 )
 
 type SampleTictailAuth struct {
-	clientId string
+	clientId     string
 	clientSecret string
-	code string
-	accessToken string
+	code         string
+	accessToken  string
 }
 
 type TAuth struct {
-	AccessToken string `json:"access_token"`
-	TokenType string `json:"token_type"`
-	ExpiresIn int `json:"expires_in"`
-	Store map[string]interface{} `json:"store"`
+	AccessToken string                 `json:"access_token"`
+	TokenType   string                 `json:"token_type"`
+	ExpiresIn   int                    `json:"expires_in"`
+	Store       map[string]interface{} `json:"store"`
 }
 
-func newSampleTictailAuth(clientId, clientSecret, code string) (SampleTictailAuth, error){
+func newSampleTictailAuth(clientId, clientSecret, code string) (SampleTictailAuth, error) {
 	sta := SampleTictailAuth{
 		clientId,
 		clientSecret,
@@ -39,14 +39,14 @@ func newSampleTictailAuth(clientId, clientSecret, code string) (SampleTictailAut
 func (sta SampleTictailAuth) getToken() error {
 	resp, err := http.PostForm(AUTH_URL,
 		url.Values{
-			"client_id": {sta.clientId},
+			"client_id":     {sta.clientId},
 			"client_secret": {sta.clientSecret},
-			"code": {sta.code},
-			"grant_type": {"authorization_code"},
-		});
+			"code":          {sta.code},
+			"grant_type":    {"authorization_code"},
+		})
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	var tauth TAuth 
+	var tauth TAuth
 	err = json.Unmarshal(body, tauth)
 	sta.accessToken = tauth.AccessToken
 	return err
